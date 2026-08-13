94ef3259951291d03f542cf1dfa2b5cf29e9afa4
Muhammad Ibrar <abrargulzar2004@gmail.com>
2026-08-13 22:21:52 +0500

    Fix catch-all route to exclude build assets

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/{any}', function () {
     return file_get_contents(public_path('build/index.html'));
-})->where('any', '^(?!api).*$');
+})->where('any', '^(?!api|build).*$');
