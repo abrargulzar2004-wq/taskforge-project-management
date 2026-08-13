f6f5c22bee99811cf64e802ee4d1b15ff9ba8ebe
Muhammad Ibrar <abrargulzar2004@gmail.com>
2026-08-13 12:25:54 +0500

    Run database seeder automatically on every production deploy

--- docker/start.sh
+++ docker/start.sh
@@ -5,4 +5,5 @@ sed -i "s/:80>/:${PORT:-80}>/g" /etc/apache2/sites-available/000-default.conf
 php artisan config:cache
 php artisan route:cache
 php artisan migrate --force
+php artisan db:seed --force
 apache2-foreground
